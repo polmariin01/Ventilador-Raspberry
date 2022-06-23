6188efcaf14897f46210ceec0304f394ff68bbbd
polmariin01 <pol.marin.gargallo@gmail.com>
2022-06-23 17:52:06 +0200

    otro canmbio de pin xd

--- ventilador.sh
+++ ventilador.sh
@@ -5,7 +5,7 @@ BASE_GPIO_PATH=/sys/class/gpio
 
 # Assign names to GPIO pin number
 # The fan will be assigned in GPIO11 and the GND will be the pin next to it
-FAN=2
+FAN=4
 
 # Assign names to states
 ON="1"
